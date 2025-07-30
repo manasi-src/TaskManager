82ede926ab9e1c12fc40aaf294d23ca13d49c97f
Manasi Patil <manasi.patil@centralogic.net>
2025-07-30 15:14:17 +0530

    Added API and Core files of Task Manager to UnderProgress Branch

---
+++ TaskManager.Core/Data/TaskManagerContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Core.Models;
+using Task = TaskManager.Core.Models.Task;
+
+namespace TaskManager.Core.Data
+{
+    public class TaskManagerContext : DbContext
+    {
+        public TaskManagerContext(DbContextOptions<TaskManagerContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>()
+                .HasOne(t => t.User)
+                .WithMany(u => u.Tasks)
+                .HasForeignKey(t => t.UserId);
+
+            modelBuilder.Entity<Task>()
+                .HasOne(t => t.Category)
+                .WithMany(c => c.Tasks)
+                .HasForeignKey(t => t.CategoryId);
+        }
+    }
+}
---
+++ TaskManager.Core/Models/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Core.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public ICollection<Task> Tasks { get; set; }
+    }
+
+    // Task.cs
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public TaskStatus Status { get; set; } = TaskStatus.Pending;
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+
+    public enum TaskStatus
+    {
+        Pending,
+        InProgress,
+        Completed
+    }
+
+    // Category.cs
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Task> Tasks { get; set; }
+    }
+}
